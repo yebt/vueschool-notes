a8aa2e6a4b8d80c3b6fcbddb11f8e1f772f7f0c2
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-01 01:44:32 -0500

    feat: add fetch destinations from the server

--- dummy-servers/src/index.ts
+++ dummy-servers/src/index.ts
@@ -1,9 +1,49 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+// ---
+interface Experience {
+  name: string;
+  slug: string;
+  image: string;
+  description: string;
+}
+interface Destination {
+  id: number;
+  name: string;
+  slug: string;
+  image: string;
+  description: string;
+  experiences: Experience[];
+}
+interface Destinations {
+  destinations: Destination[];
+}
+// ---
+const dataFile = "src/destinations.json";
+// ---
+const app = new Hono();
+app.use(cors());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.get("api/v1/destinations/:slug", async (c) => {
+  const { slug } = c.req.param();
+  const dataFromFile = Bun.file(dataFile);
+  const content = (await dataFromFile.json()) as Destinations;
+  const destination = content.destinations.find(
+    (dest: Destination) => dest.slug === slug,
+  );
+  if (!destination) {
+    return c.notFound();
+  }
+
+  return c.json(destination);
+});
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
