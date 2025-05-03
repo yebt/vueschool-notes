a1c8c7b3246a7b91148e54fb2664c99174840b10
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-03 18:54:50 -0500

    feat: wrap home tag

--- vue-router/src/views/Home.vue
+++ vue-router/src/views/Home.vue
@@ -5,23 +5,25 @@ const destinations = sourceData.destinations
 </script>
 
 <template>
-  <section class="">
-    <hgroup>
-      <h1>All Destinations</h1>
-      <p>This is the home view</p>
-    </hgroup>
-  </section>
-  <hr />
-  <section class="gridder">
-    <!-- hope -->
-    <router-link class="" v-for="destiny in destinations" :key="destiny.id"
-      :to="{ name: 'destination.show', params: { id: destiny.id, slug: destiny.slug } }">
-      <article class="linker-card">
-        <header>
-          <h4>{{ destiny.name }}</h4>
-        </header>
-        <img :src="`/images/${destiny.image}`" :alt="destiny.name" />
-      </article>
-    </router-link>
-  </section>
+  <div>
+    <section class="">
+      <hgroup>
+        <h1>All Destinations</h1>
+        <p>This is the home view</p>
+      </hgroup>
+    </section>
+    <hr />
+    <section class="gridder">
+      <!-- hope -->
+      <router-link class="" v-for="destiny in destinations" :key="destiny.id"
+        :to="{ name: 'destination.show', params: { id: destiny.id, slug: destiny.slug } }">
+        <article class="linker-card">
+          <header>
+            <h4>{{ destiny.name }}</h4>
+          </header>
+          <img :src="`/images/${destiny.image}`" :alt="destiny.name" />
+        </article>
+      </router-link>
+    </section>
+  </div>
 </template>
