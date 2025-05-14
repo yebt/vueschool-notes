a6685ddfe648ded9ed74612c09bda5359fdf0c5e
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-13 23:53:47 -0500

    feat: fetch the projects

--- vue-3-masterclass-2024/src/pages/index.vue
+++ vue-3-masterclass-2024/src/pages/index.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-
-const getProjects = async () => {
-  const { data, error } = await supabase
-    .from('projects')
-    .select()
-}
 
 </script>
 
 <template>
   <div>
     <h1>Home view</h1>
-    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to project 1</RouterLink>
+    <!-- <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to project 1</RouterLink> -->
+    <RouterLink :to="{name: '/projects/'}" >Go to projects</RouterLink>
   </div>
 </template>
--- vue-3-masterclass-2024/src/pages/projects/index.vue
+++ vue-3-masterclass-2024/src/pages/projects/index.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
+
+import { supabase } from '@/lib/supabaseClient'
+import type { Project } from '@/types/Project';
+import { ref } from 'vue';
+
+const projectsList = ref<Project[] | null>(null)
+
+  // NOTE: IIFE: Inmediatly Invoke Function Expression
+  ; (async () => {
+    const { data, error } = await supabase
+      .from('projects')
+      .select()
+
+    if (error) console.log('ERROR', error)
+
+    projectsList.value = data ?? []
+
+    console.log('projects:', projectsList.value)
+  })()
+
 </script>
 
 <template>
   <div>
     <h1>Projects view</h1>
     <RouterLink to="/">Go to Home</RouterLink>
+
+    <ul v-if="projectsList && projectsList.length > 0">
+      <li v-for="(project) in projectsList" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
+    <section v-else>
+      -- No results --
+    </section>
   </div>
 </template>
