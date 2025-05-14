3f5c65295170aba2dac74cc7c3ed240e59530150
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-14 01:05:34 -0500

    feat: add tasks route to list tasks

---
+++ vue-3-masterclass-2024/src/pages/tasks/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient';
+import type { Tables } from '@database/types';
+import { ref } from 'vue';
+
+
+const tasksList = ref<Tables<'tasks'>[] | null>(null)
+
+  // NOTE: IIFE: Inmediatly Invoke Function Expression
+  ; (async () => {
+    const { data, error } = await supabase
+      .from('tasks')
+      .select()
+
+    if (error) console.log('ERROR', error)
+
+    tasksList.value = data ?? []
+
+    console.log('projects:', tasksList.value)
+  })()
+</script>
+
+<template>
+  <div>
+    <h1>Tasks list</h1>
+    <ul v-if="tasksList && tasksList.length > 0">
+      <li v-for="(tasksEl) in tasksList" :key="tasksEl.id">
+        <b> TSK: </b> <i>{{ tasksEl.name }}</i>
+      </li>
+    </ul>
+    <section v-else>
+      -- No tasks --
+    </section>
+  </div>
+</template>
--- vue-3-masterclass-2024/typed-router.d.ts
+++ vue-3-masterclass-2024/typed-router.d.ts
@@ -23,5 +23,6 @@ declare module 'vue-router/auto-routes' {
     '/projects/': RouteRecordInfo<'/projects/', '/projects', Record<never, never>, Record<never, never>>,
     '/projects/[...catchAll]': RouteRecordInfo<'/projects/[...catchAll]', '/projects/:catchAll(.*)', { catchAll: ParamValue<true> }, { catchAll: ParamValue<false> }>,
     '/projects/[id]': RouteRecordInfo<'/projects/[id]', '/projects/:id', { id: ParamValue<true> }, { id: ParamValue<false> }>,
+    '/tasks/': RouteRecordInfo<'/tasks/', '/tasks', Record<never, never>, Record<never, never>>,
   }
 }
