4afc8551518798cf9b2c6fcd2ce7550d3cb0a776
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-13 21:52:35 -0500

    feat: add the base link, migrate and reset commands in scripts and include a project scheme for migration

---
+++ vue-3-masterclass-2024/supabase/migrations/20250514022253_project-schema.sql
@@ -0,0 +1,17 @@
+-- CREATE A PROJECTS
+
+DROP TABLE IF EXISTS projects;
+DROP TYPE IF EXISTS current_status;
+CREATE TYPE current_status AS ENUM('in-progress', 'completed');
+
+CREATE TABLE 
+  projects (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name TEXT NOT NULL,
+    slug TEXT UNIQUE NOT NULL,
+    status current_status DEFAULT 'in-progress' NOT NULL,
+    collaborators TEXT ARRAY DEFAULT ARRAY[]::VARCHAR[] NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
+  );
+
