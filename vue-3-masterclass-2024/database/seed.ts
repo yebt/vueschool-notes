061aa424afa72bfebf59aab488edf106844774d2
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-13 22:12:58 -0500

    doc

--- vue-3-masterclass-2024/database/seed.ts
+++ vue-3-masterclass-2024/database/seed.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   process.env.VITE_SUPABASE_URL ?? '',
-  process.env.SERVICE_ROLE_KEY ?? '',
+  process.env.SERVICE_ROLE_KEY ?? '', // Use a service key cause the supabase key because row level security add a constrains to security
 )
 
 console.log(supabase);
