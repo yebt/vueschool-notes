a37c22e06c3946c2a927edb3666fe04f05bec169
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-15 23:34:22 -0500

    feat: add tailwindcss config plugin in vite settings

--- vue-3-masterclass-2024/vite.config.ts
+++ vue-3-masterclass-2024/vite.config.ts
@@ -4,10 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vueRouter from 'unplugin-vue-router/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vueRouter(),
     vue(),
     vueDevTools(),
