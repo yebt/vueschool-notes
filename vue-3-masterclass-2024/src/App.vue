afa5c3e9395fa7146b261662d675f9ed9a5f654c
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-16 00:04:20 -0500

    feat: initial  work in header

--- vue-3-masterclass-2024/src/App.vue
+++ vue-3-masterclass-2024/src/App.vue
@@ -2,6 +2,14 @@
 </script>
 
 <template>
+  <header>
+    <nav class="h-16 border-b bg-muted/40 flex justify-between px-6 items-center">
+      <form class="w-full max-w-96" >
+        <input type="text" class="w-full pl-8 bg-background" placeholder="Search ...">
+      </form>
+      <div class="w-8 h-8 rounded-full bg-white"></div>
+    </nav>
+  </header>
   <main>
     <RouterView />
   </main>
