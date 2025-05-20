70e43d92ad54ab1ba7bff442164c59bee564cebb
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-20 03:10:13 -0500

    feat: remove imports of core cause use 'unplugin-auto-import'

--- vue-3-masterclass-2024/src/pages/projects/index.vue
+++ vue-3-masterclass-2024/src/pages/projects/index.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 
 import { supabase } from '@/lib/supabaseClient'
-import { h, ref } from 'vue';
 import type { Tables } from '@database/types'
 import type { ColumnDef } from '@tanstack/vue-table';
 import DataTable from '@/components/ui/data-table/DataTable.vue';
--- vue-3-masterclass-2024/src/pages/tasks/index.vue
+++ vue-3-masterclass-2024/src/pages/tasks/index.vue
@@ -3,7 +3,6 @@ import { supabase } from '@/lib/supabaseClient';
 import DataTable from '@/components/ui/data-table/DataTable.vue';
 import type { Tables } from '@database/types';
 import type { ColumnDef } from '@tanstack/vue-table';
-import { h, ref } from 'vue';
 import { RouterLink } from 'vue-router';
 
 
