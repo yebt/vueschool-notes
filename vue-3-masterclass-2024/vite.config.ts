2d0f87ee8ed92e4a38746e1f42effb09a5097d54
Yahir <yahir.eduardo.bravo@gmail.com>
2025-05-20 03:03:19 -0500

    feat: install unplugin-auto-import to ommit some imports of core

--- vue-3-masterclass-2024/vite.config.ts
+++ vue-3-masterclass-2024/vite.config.ts
@@ -5,15 +5,37 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import vueRouter from 'unplugin-vue-router/vite'
 import tailwindcss from '@tailwindcss/vite'
+import AutoIport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    AutoIport({
+
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.vue\.[tj]sx?\?vue/, // .vue (vue-loader with experimentalInlineMatchResource enabled)
+        /\.md$/, // .md
+      ],
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        'vue-router',
+        // custom
+        {}
+      ],
+      dts: true,
+      viteOptimizeDeps: true,
+    }),
     tailwindcss(),
     vueRouter(),
     vue({
-      template:{
-        compilerOptions:{
+      template: {
+        compilerOptions: {
           isCustomElement: element => element.startsWith('iconify-icon')
         }
       }
