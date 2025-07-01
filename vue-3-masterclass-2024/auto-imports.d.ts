0639fa5eff68224c81176dcedb8cf2dd79623f3f
Yahir <yahir.eduardo.bravo@gmail.com>
2025-06-30 23:23:05 -0500

    feat: add new import for uselink

--- vue-3-masterclass-2024/auto-imports.d.ts
+++ vue-3-masterclass-2024/auto-imports.d.ts
@@ -60,7 +60,6 @@ declare global {
   const useCssModule: typeof import('vue')['useCssModule']
   const useCssVars: typeof import('vue')['useCssVars']
   const useId: typeof import('vue')['useId']
-  const useLink: typeof import('vue-router')['useLink']
   const useModel: typeof import('vue')['useModel']
   const usePageStore: typeof import('./src/stores/page')['usePageStore']
   const useRoute: typeof import('vue-router')['useRoute']
