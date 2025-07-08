30db78829f3b5bd228d1498c1839ae9c48cfca50
Yahir <yahir.eduardo.bravo@gmail.com>
2025-07-08 00:52:12 -0500

    feat: add last thing in the task details view

--- vue-3-masterclass-2024/src/pages/projects/[slug].vue
+++ vue-3-masterclass-2024/src/pages/projects/[slug].vue
@@ -30,30 +30,25 @@ await getProjetWithDetails()
 </script>
 
 <template>
-  <Table>
+  <Table v-if="project">
     <TableRow>
       <TableHead> Name </TableHead>
-      <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+      <TableCell> {{ project.name }} </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Description </TableHead>
-      <TableCell>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure qui tempora ex nihil, ab
-        reprehenderit dolorem sunt veritatis perferendis? Repudiandae quis velit quasi ab natus quia
-        ratione voluptas deserunt labore sed distinctio nam fuga fugit vero voluptates placeat
-        aperiam, saepe excepturi eos harum consectetur doloremque perspiciatis nesciunt! Incidunt,
-        modi.
-      </TableCell>
+      <TableCell> {{ project.description }} </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Status </TableHead>
-      <TableCell>In progress</TableCell>
+      <TableCell>{{ project.status }}</TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Collaborators </TableHead>
       <TableCell>
         <div class="flex">
-          <Avatar class="-mr-4 border border-primary hover:scale-110 transition-transform" v-for="n in 5" :key="n">
+          <Avatar class="-mr-3 border border-primary hover:scale-110 hover:z-2 transition-transform"
+            v-for="collab in project.collaborators" :key="collab">
             <RouterLink class="w-full h-full flex items-center justify-center" to="">
               <AvatarImage src="" alt="" />
               <AvatarFallback> </AvatarFallback>
@@ -64,7 +59,7 @@ await getProjetWithDetails()
     </TableRow>
   </Table>
 
-  <section class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
+  <section v-if="project" class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
     <div class="flex-1">
       <h2>Tasks</h2>
       <div class="table-container">
@@ -77,7 +72,7 @@ await getProjetWithDetails()
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow v-for="n in 5" :key="n">
+            <TableRow v-for="task in project.tasks" :key="task.id">
               <TableCell> Lorem ipsum dolor sit amet. </TableCell>
               <TableCell> In progress </TableCell>
               <TableCell> 22/08/2024 </TableCell>
