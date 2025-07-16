7c5a964ce8560efb386889664e9a6e83cccfaa43
Yahir <yahir.eduardo.bravo@gmail.com>
2025-07-16 03:09:01 -0500

    feat: allow register a user and make a new profile

--- vue-3-masterclass-2024/src/pages/register.vue
+++ vue-3-masterclass-2024/src/pages/register.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
 const formData = ref({
   username: '',
   fistName: '',
@@ -7,6 +9,30 @@ const formData = ref({
   password: '',
   confirmPassword: '',
 })
+
+const router = useRouter()
+
+const sigup = async () => {
+  const { data, error } = await supabase.auth.signUp({
+    email: formData.value.email,
+    password: formData.value.password,
+  })
+
+  if (error) return console.log(error)
+
+  if (data.user) {
+    // success creating a enw user
+    const { error } = await supabase.from('profiles').insert({
+      id: data.user.id,
+      username: formData.value.username,
+      full_name: `${formData.value.fistName} ${formData.value.lastName}`,
+    })
+
+    if (error) console.log('Profiles err:', error)
+
+    router.push('/')
+  }
+}
 </script>
 
 <template>
@@ -23,7 +49,7 @@ const formData = ref({
           <Button variant="outline" class="w-full"> Register with Google </Button>
           <Separator label="Or" />
         </div>
-        <form class="grid gap-4">
+        <form class="grid gap-4" @submit.prevent="sigup">
           <div class="grid gap-2">
             <Label id="username" class="text-left">Username</Label>
             <Input
