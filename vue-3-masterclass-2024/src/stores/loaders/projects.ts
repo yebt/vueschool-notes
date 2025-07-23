dda70a0528e7f5a1f4401c61342c681bb8195eaa
Yahir <yahir.eduardo.bravo@gmail.com>
2025-07-22 23:01:06 -0500

    feat: add useMemoize to cache the response in the projectts, just fetch data once

--- vue-3-masterclass-2024/src/stores/loaders/projects.ts
+++ vue-3-masterclass-2024/src/stores/loaders/projects.ts
@@ -1,13 +1,18 @@
 import { projectsQuery, type Projects } from '@/utils/supaQueries'
+import { useMemoize } from '@vueuse/core'
 
 export const useProjectsStore = defineStore('project-store', () => {
   const projectsList = ref<Projects | null>(null)
 
+  // wrap promise or composable
+  const loadProjects = useMemoize(async (_: string) => await projectsQuery)
+
   const getProjects = async () => {
+    // NOTE: not need cause useMemoize
+    // if (projectsList.value?.length) return
+    // const { data, error, status } = await projectsQuery
 
-    if (projectsList.value?.length) return
-
-    const { data, error, status } = await projectsQuery
+    const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
     projectsList.value = data ?? []
