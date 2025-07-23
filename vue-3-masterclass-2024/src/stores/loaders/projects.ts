dda1767c1fbbf8db49e0abf06b0e0f7c0c4007c3
Yahir <yahir.eduardo.bravo@gmail.com>
2025-07-22 23:50:35 -0500

    feat: update stale data with fresh data

--- vue-3-masterclass-2024/src/stores/loaders/projects.ts
+++ vue-3-masterclass-2024/src/stores/loaders/projects.ts
@@ -2,21 +2,19 @@ import { projectsQuery, type Projects } from '@/utils/supaQueries'
 import { useMemoize } from '@vueuse/core'
 
 export const useProjectsStore = defineStore('project-store', () => {
-  const projectsList = ref<Projects | null>(null)
+  const projectsList = ref<Projects>([])
 
   // wrap promise or composable
   const loadProjects = useMemoize(async (_: string) => await projectsQuery)
 
   const validateCache = () => {
     if (projectsList.value?.length) {
-      projectsQuery.then(({ data }) => {
+      projectsQuery.then(({ data, error }) => {
         if (JSON.stringify(projectsList.value) === JSON.stringify(data)) {
-          console.log('Cached and fresh data matched')
           return
         } else {
-          console.log('some has changed')
-          // Remove the old data
           loadProjects.delete('projects')
+          if (!error && data) projectsList.value = data
         }
       })
     }
@@ -31,7 +29,7 @@ export const useProjectsStore = defineStore('project-store', () => {
 
     if (error) useErrorStore().setError({ error, customCode: status })
 
-    projectsList.value = data ?? []
+    if (data) projectsList.value = data
 
     validateCache()
   }
