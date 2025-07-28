45c08490e3dff620dcbaefab8e2e64d767eb5679
Yahir <yahir.eduardo.bravo@gmail.com>
2025-07-27 21:01:56 -0500

    feat: add component text area to edit in place for the projects

---
+++ vue-3-masterclass-2024/src/components/AppInPlaceEdit/AppInPlaceEditAreaTextt.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const value = defineModel<string | null>()
+const emit = defineEmits(['commit'])
+</script>
+
+<template>
+  <textarea
+    class="w-full p-1 bg-transparent focus:outline-none focus:border-none focus:bg-stone-200 dark:focus:bg-gray-800 focus:rounded-md field-sizing-content"
+    v-model="value"
+    @blur="emit('commit')"
+    @keyup.enter="($event.target as HTMLInputElement).blur()"
+  ></textarea>
+</template>
+
+<style scoped></style>
