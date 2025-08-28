5061e27a16b16135388112206aa379c57b148fff
Yahir <yahir.eduardo.bravo@gmail.com>
2025-08-28 01:58:24 -0500

    feat: add a drop down for the task and project creation button

--- vue-3-masterclass-2024/src/components/Layout/AsideBar.vue
+++ vue-3-masterclass-2024/src/components/Layout/AsideBar.vue
@@ -38,7 +38,6 @@ const accountLinks: LinkProp[] = [
   },
 ]
 
-
 const router = useRouter()
 
 const executeAction = async (linkTitle: string) => {
@@ -51,7 +50,6 @@ const executeAction = async (linkTitle: string) => {
     const { logout } = await import('@/utils/supaAuth')
     const isLoggedOut = await logout()
     if (isLoggedOut) router.push('/login')
-
   }
 }
 </script>
@@ -65,9 +63,17 @@ const executeAction = async (linkTitle: string) => {
         <iconify-icon icon="lucide:menu"></iconify-icon>
       </Button>
 
-      <Button variant="outline" size="icon" class="w-8 h-8">
-        <iconify-icon icon="lucide:plus"></iconify-icon>
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant="outline" size="icon" class="w-8 h-8">
+            <iconify-icon icon="lucide:plus"></iconify-icon>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem> Task </DropdownMenuItem>
+          <DropdownMenuItem> Project </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
 
     <nav class="flex flex-col gap-2 justify-between h-full relative">
