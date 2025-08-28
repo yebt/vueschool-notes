ef8362277e7a10a7e7de4dc3ed045e28ce2b23d2
Yahir <yahir.eduardo.bravo@gmail.com>
2025-08-28 01:56:18 -0500

    feat: add profile top with right profile photo

--- vue-3-masterclass-2024/src/components/Layout/TopNavbar.vue
+++ vue-3-masterclass-2024/src/components/Layout/TopNavbar.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { profile } = storeToRefs(useAuthStore())
+</script>
 <template>
   <nav class="h-16 border-b bg-muted/40 flex gap-2 justify-between px-6 items-center">
     <form class="relative h-fit w-full max-w-96">
@@ -8,20 +10,36 @@
       ></iconify-icon>
       <Input class="w-full pl-8 bg-background" type="text" placeholder="Search ..." />
     </form>
-    <DropdownMenu>
+    <DropdownMenu v-if="profile">
       <DropdownMenuTrigger>
         <Avatar>
-          <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+          <!-- <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" /> -->
+          <!-- <AvatarFallback>CN</AvatarFallback> -->
+          <AvatarImage
+            :src="profile.avatar_url || ''"
+            :alt="`${profile.full_name} profile
+picture`"
+          />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
+        <DropdownMenuItem>
+          <RouterLink
+            :to="{
+              name: '/users/[username]',
+              params: { username: profile.username },
+            }"
+            class="w-full h-full"
+          >
+            Profile
+          </RouterLink>
+        </DropdownMenuItem>
+        <!-- <DropdownMenuItem>Billing</DropdownMenuItem> -->
+        <!-- <DropdownMenuItem>Team</DropdownMenuItem> -->
+        <!-- <DropdownMenuItem>Subscription</DropdownMenuItem> -->
       </DropdownMenuContent>
     </DropdownMenu>
   </nav>
