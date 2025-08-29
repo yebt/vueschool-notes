be3a2e51488f7a08008db7b2585372499144272e
Yahir <yahir.eduardo.bravo@gmail.com>
2025-08-28 23:57:39 -0500

    feat: add a complete form for a task creation

--- vue-3-masterclass-2024/src/components/AppNew/AppNewTask.vue
+++ vue-3-masterclass-2024/src/components/AppNew/AppNewTask.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import type { CreateNewTask } from '@/types/CreateNewForm'
+
 const sheetOpen = defineModel<boolean>()
+
+const createtask = async (formData: CreateNewTask) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(console.log(formData))
+    }, 2000)
+  })
+}
+
 </script>
 
 <template>
@@ -7,9 +18,41 @@ const sheetOpen = defineModel<boolean>()
     <SheetContent>
       <SheetHeader>
         <SheetTitle> Create new Task </SheetTitle>
-        <SheetDescription>
-          <FormKit type="text" />
-        </SheetDescription>
+        <SheetDescription> Just create some new task</SheetDescription>
+
+        <FormKit type="form" @submit="createtask" submit-label="Create task">
+          <FormKit
+            type="text"
+            name="name"
+            id="name"
+            label="Name of task"
+            placeholder="My new tasks"
+          />
+          <FormKit
+            type="select"
+            name="for"
+            id="for"
+            label="For"
+            placeholder="Select a user"
+            :options="[{ label: 'Item name', value: 1 }]"
+          />
+          <FormKit
+            type="textarea"
+            name="description"
+            id="description"
+            label="Description"
+            placeholder="Task description"
+          />
+          <FormKit
+            type="select"
+            name="project"
+            id="project"
+            label="Project"
+            placeholder="Select a
+          project"
+            :options="[{ label: 'Item name', value: 1 }]"
+          />
+        </FormKit>
       </SheetHeader>
     </SheetContent>
   </Sheet>
